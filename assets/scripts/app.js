3008d9f52754295104da3b8beee44b4120155205
Hubert Bukala <hubertbukala@icloud.com>
2021-12-20 18:57:08 +0100

    added function to check results

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,30 @@
 const attackValue = 10;
 
 let maxLifeSet = 100;
-let currentMonsterBar = maxLifeSet;
+let currentMonsterHealthBar = maxLifeSet;
+let currentPlayerHealthBar = maxLifeSet
 
 adjustHealthBars(maxLifeSet);
 
+function resultCheck(monsterH, playerH){
+    if (monsterH <= 0 && playerH > 0){
+        alert('You won!');
+        resetGame(maxLifeSet);
+    } else if (playerH <= 0 && monsterH > 0){
+        alert('You Lose!');
+        resetGame(maxLifeSet);
+    } else if (monsterH <= 0 && playerH <=0){
+        alert("it's a draw !");
+    }
+}
+
 function attackTrigger() {
-    const damage = dealMonsterDamage(attackValue);
-    currentMonsterBar -= damage;
+    const damageMonster = dealMonsterDamage(attackValue);
+    currentMonsterHealthBar -= damageMonster;
+    resultCheck(currentMonsterHealthBar, currentPlayerHealthBar);
+    const damagePlayer = dealPlayerDamage(attackValue);
+    currentPlayerHealthBar -= damagePlayer;
+    resultCheck(currentMonsterHealthBar, currentPlayerHealthBar);
 }
 
 attackBtn.addEventListener('click',attackTrigger)
